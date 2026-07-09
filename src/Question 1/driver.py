8689f9c73e355f5a804cc991aad92783097ee899
Jayapradha1S <jayapradha247@gmail.com>
2026-07-09 11:47:47 +0530

    q1 list

--- src/Question 1/driver.py
+++ src/Question 1/driver.py
@@ -1,1 +1,8 @@
+from util import execute
 
+list = []
+n = int(input())
+
+for i in range(1, n + 1):
+    st = input()
+    execute(list, st)
--- src/Question 1/util.py
+++ src/Question 1/util.py
@@ -1,1 +1,19 @@
+def execute(list, st):
+    strl = st.split()
+    a = strl[0]
 
+    match(a):
+        case "append":
+            list.append(int(strl[1]))
+        case "insert":
+            list.insert(int(strl[1]), int(strl[2]))
+        case "pop":
+            list.pop()
+        case "sort":
+            list.sort()
+        case "reverse":
+            list.reverse()
+        case "remove":
+            list.remove(int(strl[1]))
+        case "print":
+            print(list)
