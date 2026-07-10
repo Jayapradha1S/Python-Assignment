aa09afe3283577487e62bdea103f3cfd322fa938
Jayapradha1S <jayapradha247@gmail.com>
2026-07-10 15:47:10 +0530

    noidea problem

---
+++ src/13.noidea/util.py
@@ -0,0 +1,10 @@
+def calculate_happiness(arr, A, B):
+    happiness = 0
+
+    for i in arr:
+        if i in A:
+            happiness += 1
+        elif i in B:
+            happiness -= 1
+
+    return happiness
